f4816e1b534650b081a7e980f1befe034e3f5a53
derek <derekle.creative@gmail.com>
2021-08-05 01:50:11 -0500

    commit tree 8d599bd1de437aa34a57129e65633aac8b43b2f5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, gift){
+    let arr = []
+    for (let i=0; i<names.length; i++){
+        arr.push("Thank you, "+ names[i] +", for the wonderful "+ gift +" gift!")
+    }
+    return arr;
+}
+
+function countDown(){
+    let countdown = 10;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    } 
+}
